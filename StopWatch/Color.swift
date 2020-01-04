7b495e111ea0b415d9d9f16cb0b91bb5b04ee6d0
ito.motoyuki <ito.motoyuki@crimsontech.jp>
2020-01-04 09:37:28 +0900

    使用する色の定義をAssets.xcassetsからUIColorに変更

---
+++ StopWatch/Color.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  StopWatch
+//
+//  Created by Motoyuki Ito on 2020/01/03.
+//  Copyright © 2020 Motoyuki Ito. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+	static var background 	= UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+	static var tabBar 		= UIColor(red: 20 / 255, green: 20 / 255, blue: 20 / 255, alpha: 1)
+	static var startBurron 	= UIColor(red: 9 / 255, green: 42 / 255, blue: 17 / 255, alpha: 1)
+	static var stopBurron 	= UIColor(red: 51 / 255, green: 14 / 255, blue: 11 / 255, alpha: 1)
+	static var lapBurron 	= UIColor(red: 51 / 255, green: 51 / 255, blue: 51 / 255, alpha: 1)
+}
