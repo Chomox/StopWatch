5a3fdda11e2010b9af68d7e7281e64d59b60fa44
ito.motoyuki <ito.motoyuki@crimsontech.jp>
2020-01-05 02:00:08 +0900

    ・startButtonText,stopButtonTextをUIColorに追加

--- StopWatch/Color.swift
+++ StopWatch/Color.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 extension UIColor {
-	static var background 	= UIColor(red: 1, green: 1, blue: 1, alpha: 1)
-	static var tabBar 		= UIColor(red: 20 / 255, green: 20 / 255, blue: 20 / 255, alpha: 1)
-	static var startBurron 	= UIColor(red: 9 / 255, green: 42 / 255, blue: 17 / 255, alpha: 1)
-	static var stopBurron 	= UIColor(red: 51 / 255, green: 14 / 255, blue: 11 / 255, alpha: 1)
-	static var lapBurron 	= UIColor(red: 51 / 255, green: 51 / 255, blue: 51 / 255, alpha: 1)
+	static var background 				= UIColor(red: 0, 			green: 0, 			blue: 0, 		alpha: 1)
+	static var tabBar 					= UIColor(red: 20 	/ 255, 	green: 20 	/ 255,	blue: 20 / 255, alpha: 1)
+	static var startButtonBackground 	= UIColor(red: 9	/ 255, 	green: 42 	/ 255,	blue: 17 / 255, alpha: 1)
+	static var startButtonText 			= UIColor(red: 48 	/ 255, 	green: 209	/ 255,	blue: 88 / 255, alpha: 1)
+	static var stopButtonBackground 	= UIColor(red: 51 	/ 255, 	green: 14 	/ 255, 	blue: 11 / 255, alpha: 1)
+	static var stopButtonText 			= UIColor(red: 255 	/ 255,	green: 69 	/ 255, 	blue: 58 / 255, alpha: 1)
+	static var lapBurron 				= UIColor(red: 51 	/ 255, 	green: 51 	/ 255, 	blue: 51 / 255, alpha: 1)
 }
