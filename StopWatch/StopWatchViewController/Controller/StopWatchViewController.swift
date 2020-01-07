f36eb5037d5734266cd7764986d8856c063f91e3
ito.motoyuki <ito.motoyuki@crimsontech.jp>
2020-01-08 01:06:29 +0900

    ・コード整形

--- StopWatch/StopWatchViewController/Controller/StopWatchViewController.swift
+++ StopWatch/StopWatchViewController/Controller/StopWatchViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class StopWatchViewController: UIViewController {
 	
-	//MARK: - Constant
+    //MARK: - Constant
     private enum ButtonState: String {
         case start = "Start"
         case stop = "Stop"
@@ -19,45 +19,45 @@ class StopWatchViewController: UIViewController {
     }
 
 	
-	//MARK: - Properties
-	private let stopWatch = StopWatch()
-	private var stopWatchView: StopWatchView?
-	private weak var timer :Timer?
+    //MARK: - Properties
+    private let stopWatch = StopWatch()
+    private var stopWatchView: StopWatchView?
+    private weak var timer :Timer?
 	
 	
-	//MARK: - Actions
-	@IBAction private func startButtonTapped(){
-		if timer == nil {
-			timer = .scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(update), userInfo: nil, repeats: true)
-			RunLoop.current.add(timer!, forMode: .common)
-			stopWatch.start()
-			
-			self.stopWatchView?.lapsTableView.reloadData()
-		} else {
-			timer?.invalidate()
-			timer = nil
-			stopWatch.stop()
-		}
-		
-		switchButtonsAppearance(state: stopWatch.state)
-	}
+    //MARK: - Actions
+    @IBAction private func startButtonTapped(){
+        if timer == nil {
+            timer = .scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+            RunLoop.current.add(timer!, forMode: .common)
+            stopWatch.start()
+            
+            self.stopWatchView?.lapsTableView.reloadData()
+        } else {
+            timer?.invalidate()
+            timer = nil
+            stopWatch.stop()
+        }
+
+        switchButtonsAppearance(state: stopWatch.state)
+    }
 	
-	@IBAction private func addButtonTapped(){
-		switch stopWatch.state {
-		case .valid:
-			stopWatch.add()
-			stopWatchView?.lapsTableView.reloadData()
-		case .invalid:
-			stopWatch.reset()
-			stopWatchView?.lapsTableView.reloadData()
-			stopWatchView?.timeLabel.text = stopWatch.time
-			switchButtonsAppearance(state: stopWatch.state)
-		default: break
-		}
-	}
+    @IBAction private func addButtonTapped(){
+        switch stopWatch.state {
+        case .valid:
+            stopWatch.add()
+            stopWatchView?.lapsTableView.reloadData()
+        case .invalid:
+            stopWatch.reset()
+            stopWatchView?.lapsTableView.reloadData()
+            stopWatchView?.timeLabel.text = stopWatch.time
+            switchButtonsAppearance(state: stopWatch.state)
+        default: break
+        }
+    }
 	
 	
-	//MARK: - Life Cycle
+    //MARK: - Life Cycle
     override func loadView() {
         super.loadView()
         self.view = StopWatchView()
@@ -80,37 +80,37 @@ class StopWatchViewController: UIViewController {
     }
 	
 	
-	//MARK: - Appearance
-	private func switchButtonsAppearance(state: StopWatch.State ){
-		switch state {
-		case .valid:
-			stopWatchView?.startButton.setTitle( ButtonState.stop.rawValue, for: .normal)
-			stopWatchView?.startButton.backgroundColor = .stopButtonBackground
-			stopWatchView?.startButton.tintColor = .stopButtonText
-			stopWatchView?.lapButton.setTitle( ButtonState.lap.rawValue, for: .normal)
-			stopWatchView?.lapButton.alpha = 1
-//			stopWatchView?.lapButton.isEnabled = true
-		case .invalid:
-			stopWatchView?.startButton.setTitle( ButtonState.start.rawValue, for: .normal)
-			stopWatchView?.startButton.backgroundColor = .startButtonBackground
-			stopWatchView?.startButton.tintColor = .startButtonText
-			stopWatchView?.lapButton.setTitle( ButtonState.reset.rawValue, for: .normal)
-		case .default:
-			stopWatchView?.startButton.setTitle( ButtonState.start.rawValue, for: .normal)
-			stopWatchView?.startButton.backgroundColor = .startButtonBackground
-			stopWatchView?.startButton.tintColor = .startButtonText
-			stopWatchView?.lapButton.setTitle( ButtonState.lap.rawValue, for: .normal)
-			stopWatchView?.lapButton.tintColor = .white
-			stopWatchView?.lapButton.alpha = 0.6
-//			stopWatchView?.lapButton.isEnabled = false
-		}
-	}
-	
-	
-	//MARK: - TimerHandler
-	@objc private func update(){
-		stopWatch.update()
-		stopWatchView?.timeLabel.text = stopWatch.time
-		stopWatchView?.lapsTableView.cellForRow(at: .init(row: 0, section: 0))?.detailTextLabel?.text = stopWatch.lapTime
-	}
+    //MARK: - Appearance
+    private func switchButtonsAppearance(state: StopWatch.State ){
+        switch state {
+        case .valid:
+            stopWatchView?.startButton.setTitle( ButtonState.stop.rawValue, for: .normal)
+            stopWatchView?.startButton.backgroundColor = .stopButtonBackground
+            stopWatchView?.startButton.tintColor = .stopButtonText
+            stopWatchView?.lapButton.setTitle( ButtonState.lap.rawValue, for: .normal)
+            stopWatchView?.lapButton.alpha = 1
+        //			stopWatchView?.lapButton.isEnabled = true
+        case .invalid:
+            stopWatchView?.startButton.setTitle( ButtonState.start.rawValue, for: .normal)
+            stopWatchView?.startButton.backgroundColor = .startButtonBackground
+            stopWatchView?.startButton.tintColor = .startButtonText
+            stopWatchView?.lapButton.setTitle( ButtonState.reset.rawValue, for: .normal)
+        case .default:
+            stopWatchView?.startButton.setTitle( ButtonState.start.rawValue, for: .normal)
+            stopWatchView?.startButton.backgroundColor = .startButtonBackground
+            stopWatchView?.startButton.tintColor = .startButtonText
+            stopWatchView?.lapButton.setTitle( ButtonState.lap.rawValue, for: .normal)
+            stopWatchView?.lapButton.tintColor = .white
+            stopWatchView?.lapButton.alpha = 0.6
+        //			stopWatchView?.lapButton.isEnabled = false
+        }
+    }
+
+
+    //MARK: - TimerHandler
+    @objc private func update(){
+        stopWatch.update()
+        stopWatchView?.timeLabel.text = stopWatch.time
+        stopWatchView?.lapsTableView.cellForRow(at: .init(row: 0, section: 0))?.detailTextLabel?.text = stopWatch.lapTime
+    }
 }
