f522a93a933a3e8cd92c67152db0f25427a9eee8
ito.motoyuki <ito.motoyuki@crimsontech.jp>
2020-02-01 23:48:36 +0900

    Fix redundant code expression

--- StopWatch/Color.swift
+++ StopWatch/Color.swift
@@ -11,7 +11,7 @@ import UIKit
 extension UIColor {
     static var background               = UIColor(red: 0,           green: 0,           blue: 0,        alpha: 1)
     static var tabBar                   = UIColor(red: 18   / 255,  green: 18   / 255,  blue: 18 / 255, alpha: 1)
-    static var tabBarTintColor          = UIColor(red: 254  / 255,  green: 159  / 255,  blue: 10 / 255, alpha: 1)
+    static var tabBarTint               = UIColor(red: 254  / 255,  green: 159  / 255,  blue: 10 / 255, alpha: 1)
     static var startButtonBackground    = UIColor(red: 9    / 255,  green: 42   / 255,  blue: 17 / 255, alpha: 1)
     static var startButtonText          = UIColor(red: 48   / 255,  green: 209  / 255,  blue: 88 / 255, alpha: 1)
     static var stopButtonBackground     = UIColor(red: 51   / 255,  green: 14   / 255,  blue: 11 / 255, alpha: 1)
