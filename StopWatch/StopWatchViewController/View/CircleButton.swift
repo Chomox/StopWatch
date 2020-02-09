cdcae765cce94392793b519102db70c0e9edbf7a
ito.motoyuki <ito.motoyuki@crimsontech.jp>
2020-02-09 23:44:59 +0900

    Add CircleButton for Start/Stop, Lap/Reset

---
+++ StopWatch/StopWatchViewController/View/CircleButton.swift
@@ -0,0 +1,13 @@
+//
+//  CircleButton.swift
+//  StopWatch
+//
+//  Created by Motoyuki Ito on 2020/02/09.
+//  Copyright © 2020 Motoyuki Ito. All rights reserved.
+//
+
+import UIKit
+
+final class CircleButton: UIButton {
+    
+}
