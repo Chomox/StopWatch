b88bc39b33cd26b33ee4cb02b4a1c6cdb0ec5f52
ito.motoyuki <ito.motoyuki@crimsontech.jp>
2020-02-17 21:48:54 +0900

    Revert "Add Life Cycle Function"
    This reverts commit 9efb7838ab8304edaa67ecc1dff65ea076cf2fc9.

--- StopWatch/StopWatchViewController/View/CircleButton.swift
+++ StopWatch/StopWatchViewController/View/CircleButton.swift
@@ -10,16 +10,4 @@ import UIKit
 
 final class CircleButton: UIButton {
     
-    //MARK: - Life Cycle
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-    }
 }
