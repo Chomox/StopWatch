ddfd65fffa55b05cf0141fbc1227d07ff0dbf132
ito.motoyuki <ito.motoyuki@crimsontech.jp>
2020-02-24 08:46:08 +0900

    Add support for Different layout depending on iOS

--- StopWatch/StopWatchViewController/View/StopWatchView.swift
+++ StopWatch/StopWatchViewController/View/StopWatchView.swift
@@ -13,6 +13,8 @@ final class StopWatchView: UIView {
     //MARK: - Constants
     private enum Size {
         static let font: CGFloat = 90
+        static let four_point_seven_Constraint_constant: (CGFloat, CGFloat) = (74,60)
+        static let six_point_one_Constraint_constant: (CGFloat, CGFloat) = (102.5,48)
     }
 
 
@@ -37,6 +39,9 @@ final class StopWatchView: UIView {
 
     @IBOutlet weak var startButton:     CircleButton!
     @IBOutlet weak var lapsTableView:   UITableView!
+    
+    @IBOutlet private weak var timeLabelTopConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var timeLabelBottomConstraint: NSLayoutConstraint!
 	
 
     //MARK: - Life Cycle
@@ -54,5 +59,20 @@ final class StopWatchView: UIView {
         let view = Bundle(for: type(of: self)).loadNibNamed(String(describing: type(of: self)), owner: self, options: nil)?.first as? UIView
         view?.frame = self.bounds
         self.addSubview(view ?? UIView())
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        //FIXME: Set dynamically
+        if #available(iOS 13, *){
+            timeLabelTopConstraint.constant = Size.six_point_one_Constraint_constant.0
+            timeLabelBottomConstraint.constant = Size.six_point_one_Constraint_constant.1
+        }
+        else {
+            timeLabelTopConstraint.constant = Size.four_point_seven_Constraint_constant.0
+            timeLabelBottomConstraint.constant = Size.four_point_seven_Constraint_constant.1
+        }
+        
     }
 }
